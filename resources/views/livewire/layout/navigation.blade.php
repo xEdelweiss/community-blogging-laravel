7e1994d8193385f716ef52813a159a4224b66247
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2024-02-03 03:50:15 +0200

    Tweaks: responsive Write post button

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -43,12 +43,12 @@ new class extends Component {
                 <a href="{{ route('post.create') }}">
                     <x-secondary-button>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                            class="mr-2 h-4 w-4">
+                            class="h-4 w-4 sm:mr-2">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                         </svg>
 
-                        {{ __('Write post') }}
+                        <span class="hidden sm:inline">{{ __('Write post') }}</span>
                     </x-secondary-button>
                 </a>
 
