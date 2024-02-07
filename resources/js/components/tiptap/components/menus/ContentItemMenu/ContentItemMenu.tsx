3b1ef2700e626f4b483d743745061d41caf0798d
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2024-02-07 15:15:54 +0200

    Update ContentItemMenu to follow the caret (draft)

--- resources/js/components/tiptap/components/menus/ContentItemMenu/ContentItemMenu.tsx
+++ resources/js/components/tiptap/components/menus/ContentItemMenu/ContentItemMenu.tsx
@@ -14,20 +14,13 @@ import { useEffect, useState } from "react";
 
 export type ContentItemMenuProps = {
     editor: Editor;
-    onToggleDebug: () => void;
+    onToggleDebug: (type: "html" | "json") => void;
 };
 
-export const ContentItemMenu = ({
-    editor,
-    onToggleDebug,
-}: ContentItemMenuProps) => {
+export const ContentItemMenu = ({ editor, onToggleDebug }: ContentItemMenuProps) => {
     const [menuOpen, setMenuOpen] = useState(false);
     const data = useData();
-    const actions = useContentItemActions(
-        editor,
-        data.currentNode,
-        data.currentNodePos,
-    );
+    const actions = useContentItemActions(editor, data.currentNode, data.currentNodePos);
 
     useEffect(() => {
         if (menuOpen) {
@@ -48,6 +41,9 @@ export const ContentItemMenu = ({
             }}
         >
             <div className="flex items-center gap-0.5">
+                <Toolbar.Button onClick={actions.handleAdd}>
+                    <Icon name="Plus" />
+                </Toolbar.Button>
                 <Popover.Root open={menuOpen} onOpenChange={setMenuOpen}>
                     <Popover.Trigger asChild>
                         <Toolbar.Button>
@@ -57,23 +53,25 @@ export const ContentItemMenu = ({
                     <Popover.Content side="bottom" align="start" sideOffset={8}>
                         <Surface className="p-2 flex flex-col min-w-[16rem]">
                             <Popover.Close>
-                                <DropdownButton onClick={onToggleDebug}>
+                                <DropdownButton onClick={() => onToggleDebug("json")}>
                                     <Icon name="Eye" />
-                                    Toggle debug
+                                    Toggle JSON debug
                                 </DropdownButton>
                             </Popover.Close>
                             <Popover.Close>
-                                <DropdownButton
-                                    onClick={actions.resetTextFormatting}
-                                >
+                                <DropdownButton onClick={() => onToggleDebug("html")}>
+                                    <Icon name="Eye" />
+                                    Toggle HTML debug
+                                </DropdownButton>
+                            </Popover.Close>
+                            <Popover.Close>
+                                <DropdownButton onClick={actions.resetTextFormatting}>
                                     <Icon name="RemoveFormatting" />
                                     Clear formatting
                                 </DropdownButton>
                             </Popover.Close>
                             <Popover.Close>
-                                <DropdownButton
-                                    onClick={actions.copyNodeToClipboard}
-                                >
+                                <DropdownButton onClick={actions.copyNodeToClipboard}>
                                     <Icon name="Clipboard" />
                                     Copy to clipboard
                                 </DropdownButton>
--- resources/js/components/tiptap/extensions/fake-pro.tsx
+++ resources/js/components/tiptap/extensions/fake-pro.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { forwardRef, useEffect } from "react";
+import { Editor } from "@tiptap/react";
+import { Node } from "@tiptap/pm/model";
 
 export interface EmojiItem {
     emoji: string;
@@ -8,6 +10,7 @@ export interface EmojiItem {
 
 export type ImageOptions = string;
 export type Language = string;
+
 export enum WebSocketStatus {
     "Disconnected" = "disconnected",
 }
@@ -25,12 +28,56 @@ export type TableOfContentsStorage = {
         textContent: string;
     }[];
 };
-export const DragHandle = ({
-    pluginKey,
-    editor,
-    onNodeChange,
-    tippyOptions,
-    children,
-}: any) => {
-    return <>{children} </>;
-};
+export const DragHandle = forwardRef<
+    HTMLDivElement,
+    {
+        pluginKey: string;
+        editor: Editor;
+        onNodeChange: (data: { node: Node | null; editor: Editor; pos: number }) => void;
+        tippyOptions: {
+            offset: [number, number];
+            zIndex: number;
+        };
+        children: React.ReactNode;
+    }
+>(({ pluginKey, editor, onNodeChange, tippyOptions, children }: any, ref) => {
+    const [position, setPosition] = React.useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        const handleNodeChange = () => {
+            const node = editor.state.doc.nodeAt(editor.state.selection.$anchor.pos);
+            const pos = editor.state.selection.anchor;
+
+            const cursorCoords = editor.view.coordsAtPos(editor.state.selection.$anchor.pos);
+            const { top, left } = cursorCoords;
+
+            // @fixme: this is a hack to fix the position of the drag handle
+            setPosition({
+                x: -100,
+                y: top + editor.view.dom.scrollTop - editor.view.dom.getBoundingClientRect().top - 10,
+            });
+
+            onNodeChange({ node, editor, pos });
+        };
+
+        editor.on("selectionUpdate", handleNodeChange);
+        handleNodeChange();
+
+        return () => {
+            editor.off("selectionUpdate", handleNodeChange);
+        };
+    }, [editor, onNodeChange]);
+
+    return (
+        <div
+            ref={ref}
+            style={{
+                position: "absolute",
+                top: `${position.y}px`,
+                left: `${position.x}px`,
+            }}
+        >
+            {children}{" "}
+        </div>
+    );
+});
