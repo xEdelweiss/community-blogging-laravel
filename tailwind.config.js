642a23deadb77a218e4eec354b9e24ac0eacc919
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2024-02-07 16:05:39 +0200

    Fix TailwindCSS config to include *.ts files

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
-        "./resources/js/**/*.{jsx,tsx}",
+        "./resources/js/**/*.{jsx,tsx,ts}",
     ],
 
     theme: {
