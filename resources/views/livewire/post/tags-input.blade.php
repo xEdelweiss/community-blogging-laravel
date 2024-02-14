c23e10a2bf741e2bea551724da2b8aef2454edbd
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2024-02-14 19:19:47 +0200

    Implement tags input with Livewire

---
+++ resources/views/livewire/post/tags-input.blade.php
@@ -0,0 +1,155 @@
+@props([
+    'disabled' => false,
+    'name' => 'tags',
+])
+
+<?php
+
+use App\Livewire\Forms\LoginForm;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Session;
+use Livewire\Attributes\Layout;
+use Livewire\Volt\Component;
+
+new class extends Component {
+    public array $tags = [];
+
+    public string $query = '';
+    public array $suggestions = [];
+
+    public function mount(array $tags): void
+    {
+        $this->tags = $tags;
+        $this->suggestions = $this->fetchTags('', $tags);
+    }
+
+    public function updatedQuery(): void
+    {
+        $suggestions = $this->fetchTags($this->query, $this->tags);
+
+        $this->suggestions = empty($suggestions) ? [$this->query] : $suggestions;
+    }
+
+    public function updatedTags(): void
+    {
+        $this->suggestions = $this->fetchTags($this->query, $this->tags);
+    }
+
+    private function fetchTags(string $query = '', array $existing = []): array
+    {
+        $existing = array_map('mb_strtolower', $existing);
+        $allTags = array_map('mb_strtolower', ['Made in Ukraine', 'Підтримка України', 'Скандали', 'Розробка', 'SaaS', 'Читоголік', 'Розслідування']);
+
+        $tags = array_filter($allTags, fn($tag) => !in_array($tag, $existing));
+
+        if (empty($query)) {
+            return $tags;
+        }
+
+        return array_values(
+            array_filter($tags, function ($suggestion) {
+                return Str::contains(Str::lower($suggestion), Str::lower($this->query));
+            }),
+        );
+    }
+}; ?>
+
+<div class="relative" x-data="{
+    key: 1,
+    open: false,
+    tags: @entangle('tags').live,
+    query: @entangle('query').live,
+    suggestions: @entangle('suggestions'),
+    addTag(tag) {
+        if (tag && this.tags.length < 5) {
+            this.tags.push(tag);
+        }
+    },
+    addQueryAsTag() {
+        this.addTag(this.query);
+    },
+    addSuggestedTag(tag) {
+        this.query = '';
+        this.addTag(tag);
+    },
+    removeLastTag() {
+        if (!this.query && this.tags.length > 0) {
+            this.tags.pop();
+        }
+    },
+    removeTag(tag) {
+        this.tags = this.tags.filter(t => t !== tag);
+    }
+}" @click.outside="open = false"
+    @close.stop="open = false">
+    <div class="flex flex-row">
+        <template x-for="tag in tags">
+            <input type="hidden" name="{{ $name }}[]"
+                :value="tag.toLowerCase()" />
+        </template>
+
+        <!--    Value list    -->
+        <div class="flex flex-wrap gap-2 text-sm">
+            <template x-for="tag in tags">
+                <a href="#"
+                    class="group flex pt-2 align-baseline hover:text-primary-dark"
+                    @click.prevent="removeTag(tag)">
+                    <span class="hidden group-hover:inline-block">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="inline-block h-3 w-3">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+                    </span>
+                    <span class="inline-block group-hover:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor"
+                            class="inline-block h-3 w-3 text-primary-dark">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5" />
+                        </svg>
+
+                    </span>
+                    <span x-text="tag.toLowerCase()"></span>
+                </a>
+            </template>
+
+            <!--    Input element    -->
+            <div>
+                <input aria-label="{{ __('Add up to 5 tags...') }}"
+                    :disabled="tags.length >= 5"
+                    class="border-none px-0 pt-2 text-sm text-gray-900 caret-black shadow-none focus:border-none focus:outline-none focus:ring-0 dark:text-gray-300 dark:caret-white"
+                    x-ref="input" @focus="open=(tags.length < 5)"
+                    type="text" x-model.debounce="query"
+                    @keydown.enter.prevent="addQueryAsTag()"
+                    @keydown.backspace="removeLastTag()"
+                    :placeholder="tags.length < 5 ? '{{ __('Add up to 5 tags...') }}' :
+                        '{{ __('Max 5 tags used') }}'" />
+
+                <!--    Suggestions list    -->
+                <div x-show="open"
+                    x-transition:enter="transition ease-out duration-200"
+                    x-transition:enter-start="opacity-0 scale-95"
+                    x-transition:enter-end="opacity-100 scale-100"
+                    x-transition:leave="transition ease-in duration-75"
+                    x-transition:leave-start="opacity-100 scale-100"
+                    x-transition:leave-end="opacity-0 scale-95"
+                    class="absolute z-50 mt-2 w-48 rounded-md shadow-lg ltr:origin-top-right rtl:origin-top-left"
+                    style="display: none;">
+                    <div
+                        class="rounded-md bg-white py-1 ring-1 ring-black ring-opacity-5 dark:bg-gray-700">
+                        <template x-for="suggestion in suggestions">
+                            <a class='block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-300 dark:hover:bg-gray-800 dark:focus:bg-gray-800'
+                                x-text="suggestion.toLowerCase()"
+                                @click.prevent="addSuggestedTag(suggestion)"></a>
+                        </template>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,21 +1,28 @@
 <x-main-layout x-data="{ addUrlOpen: true, url: null, postContent: { type: 'doc', content: [] } }" title="✏️ {{ __('New post') }}">
     {{-- content --}}
-    <form class="space-y-4 bg-white p-6 shadow-sm dark:bg-gray-800 sm:rounded-lg" action="{{ route('post.store') }}" method="post">
+    <form class="space-y-4 bg-white p-6 shadow-sm dark:bg-gray-800 sm:rounded-lg"
+        action="{{ route('post.store') }}" method="post">
         <input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
         <div class="flex flex-col">
-            <x-minimal-input class="w-full pb-0 text-4xl" placeholder="{{ __('Your catchy title..') }}" name="title" />
-            <x-tags-input class="w-full text-sm" placeholder="{{ __('Add up to 5 tags..') }}" name="tags" />
+            <x-minimal-input class="w-full pb-0 text-4xl"
+                placeholder="{{ __('Your catchy title..') }}" name="title" />
+            <livewire:post.tags-input :tags="['Переклад', 'Хмарні Технології', 'Скандали']" />
         </div>
 
         <div class="mb-4 flex flex-col gap-3">
-            <div class="flex items-center gap-3" :class="{ 'hidden': !addUrlOpen }">
-                <x-text-input class="w-full" placeholder="{{ __('Your URL goes here..') }}" name="url" x-model.debounce.500ms="url" />
+            <div class="flex items-center gap-3"
+                :class="{ 'hidden': !addUrlOpen }">
+                <x-text-input class="w-full"
+                    placeholder="{{ __('Your URL goes here..') }}"
+                    name="url" x-model.debounce.500ms="url" />
 
                 <x-minimal-button x-on:click.prevent="addUrlOpen = false">
-                    <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M6 18 18 6M6 6l12 12" />
                     </svg>
                 </x-minimal-button>
             </div>
@@ -25,7 +32,8 @@
 
         <div class="px-2 text-gray-900 dark:text-gray-100 sm:px-0">
             <div x-post-editor="postContent">Loading..</div>
-            <input :value="JSON.stringify(postContent)" type="hidden" name="content" />
+            <input :value="JSON.stringify(postContent)" type="hidden"
+                name="content" />
         </div>
     </form>
 
@@ -48,10 +56,12 @@
             </div>
 
             <div class="flex w-full flex-col gap-y-2">
-                <img src="https://placehold.it/600x300" alt="" class="w-full cursor-pointer rounded" />
+                <img src="https://placehold.it/600x300" alt=""
+                    class="w-full cursor-pointer rounded" />
 
                 <x-secondary-button class="flex w-full justify-center">
-                    <svg class="mr-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    <svg class="mr-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
@@ -61,10 +71,14 @@
                 </x-secondary-button>
 
                 <template x-if="addUrlOpen">
-                    <x-secondary-button class="flex w-full justify-center" @click.prevent="addUrlOpen = false">
-                        <svg class="mr-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    <x-secondary-button class="flex w-full justify-center"
+                        @click.prevent="addUrlOpen = false">
+                        <svg class="mr-2 h-4 w-4"
+                            xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M6 18 18 6M6 6l12 12" />
                         </svg>
 
                         <span>{{ __('Remove URL') }}</span>
@@ -72,8 +86,11 @@
                 </template>
 
                 <template x-if="! addUrlOpen">
-                    <x-secondary-button class="flex w-full justify-center" @click.prevent="addUrlOpen = true">
-                        <svg class="mr-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    <x-secondary-button class="flex w-full justify-center"
+                        @click.prevent="addUrlOpen = true">
+                        <svg class="mr-2 h-4 w-4"
+                            xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
@@ -83,8 +100,10 @@
                     </x-secondary-button>
                 </template>
 
-                <x-primary-button class="flex w-full justify-center" disabled x-bind:disabled="postContent.content.length < 2">
-                    <svg class="mr-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                <x-primary-button class="flex w-full justify-center" disabled
+                    x-bind:disabled="postContent.content.length < 2">
+                    <svg class="mr-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
@@ -132,7 +151,8 @@
                     {{ __('Editor tips:') }}
                 </p>
                 <ul class="ms-2 list-inside list-disc">
-                    <li>{{ __('To exit from code block use Shift+Enter') }}</li>
+                    <li>{{ __('To exit from code block use Shift+Enter') }}
+                    </li>
                 </ul>
             </div>
         </div>
