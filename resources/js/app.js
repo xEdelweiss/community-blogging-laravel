dcc84447bd310d677961850aa2fefc6887d10d67
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2024-02-25 13:42:13 +0200

    Improve Embed Links UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,12 @@ import usePostEditor from "./usePostEditor.jsx";
 import useEmbed from "./useEmbed.js";
 import useAutosize from "./useAutosize.js";
 import useLimitIndicator from "./useLimitIndicator.js";
+import useHiddenFailedImage from "./useHiddenFailedImage.js";
 
 usePostEditor();
 useAutosize();
 useLimitIndicator();
 useEmbed();
+useHiddenFailedImage();
 
 Livewire.start();
--- resources/views/embed/link.blade.php
+++ resources/views/embed/link.blade.php
@@ -1,24 +1,36 @@
+@php
+    $title = $meta->title ?? $meta->url;
+    $description = $meta->description;
+
+    if (!$description && $title !== $meta->url) {
+        $description = $meta->url;
+    }
+@endphp
 <figure provider-name="{{ $meta->providerName }}"
-    class="flex flex-col items-center justify-start gap-4 rounded-xl border p-4 sm:flex-row sm:items-start">
+    class="flex flex-col items-stretch justify-start gap-4 rounded-xl border p-4 sm:flex-row">
     @if ($meta->imageUrl)
-        <img class="h-24 w-full rounded object-cover sm:w-auto sm:max-w-[30%] sm:object-contain"
-            src="{{ $meta->imageUrl }}" title="{{ $meta->title }}"
-            alt="{{ $meta->title }}">
+        <img x-hide-if-failed
+            class="h-24 w-full rounded object-cover sm:w-auto sm:max-w-[30%] sm:object-contain"
+            src="{{ $meta->imageUrl }}" title="{{ $title }}"
+            alt="{{ $title }}">
     @endif
 
     <div class="flex flex-1 flex-col gap-2 text-left">
         <figcaption>
-            <p class="text-sm font-semibold">{{ $meta->title }}</p>
+            <p class="text-sm font-semibold">{{ $title }}</p>
         </figcaption>
 
-        @if ($meta->title !== $meta->description)
-            <p class="line-clamp-2 flex-1 text-sm">{{ $meta->description }}</p>
+        @if ($description && $description !== $title)
+            <p class="line-clamp-2 flex-1 text-sm">
+                {{ $description }}
+            </p>
         @endif
 
         <div class="flex justify-end gap-1 opacity-75">
-            <img class="h-5 w-5 rounded" src="{{ $meta->iconUrl }}"
-                alt="${meta.provider}" title="{{ $meta->providerName }}">
-            <span class="text-sm">{{ $meta->providerName }}</span>
+            <img x-hide-if-failed class="h-5 w-5 rounded"
+                src="{{ $meta->iconUrl }}" alt="${meta.provider}"
+                title="{{ $meta->providerName }}">
+            <span class="pt-[0.125rem] text-sm">{{ $meta->providerName }}</span>
         </div>
     </div>
 </figure>
