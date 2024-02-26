fb2c1a4ba5498110e90db9a18ed2743296c5e6d0
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2024-02-26 02:07:36 +0200

    Lost Post render - render intro to paragraphs - temporarily disable Table of Content extension

--- resources/js/components/tiptap/extensions/SlashCommand/groups.ts
+++ resources/js/components/tiptap/extensions/SlashCommand/groups.ts
@@ -80,17 +80,17 @@ export const GROUPS: Group[] = [
                     editor.chain().focus().setImageUpload().run();
                 },
             },
-            {
-                name: "toc",
-                label: "Table of Contents",
-                iconName: "Book",
-                aliases: ["outline"],
-                description: "Insert a table of contents",
-                shouldBeHidden: (editor) => editor.isActive("columns"),
-                action: (editor) => {
-                    editor.chain().focus().insertTableOfContents().run();
-                },
-            },
+            // {
+            //     name: "toc",
+            //     label: "Table of Contents",
+            //     iconName: "Book",
+            //     aliases: ["outline"],
+            //     description: "Insert a table of contents",
+            //     shouldBeHidden: (editor) => editor.isActive("columns"),
+            //     action: (editor) => {
+            //         editor.chain().focus().insertTableOfContents().run();
+            //     },
+            // },
         ],
     },
 ];
